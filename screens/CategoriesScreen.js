bc23f2bbea8fe39474f9d357472b721b5493b507
Chris Paterson <chrispaterson22@gmail.com>
2021-03-08 13:42:54 +0000

    CategoryGridTile component and styling

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -8,22 +8,20 @@ import {
 } from 'react-native'
 
 import { CATEGORIES } from '../data/dummy-data'
+import CategoryGridTile from '../components/CategoryGridTile'
 
 const CategoriesScreen = props => {
 	const renderGridItem = itemData => {
 		return (
-			<TouchableOpacity
-				style={styles.gridItem}
-				onPress={() =>
+			<CategoryGridTile
+				title={itemData.item.title}
+				color={itemData.item.color}
+				onSelect={() => {
 					props.navigation.navigate('CategoryMeals', {
 						categoryId: itemData.item.id,
 					})
-				}
-			>
-				<View>
-					<Text>{itemData.item.title}</Text>
-				</View>
-			</TouchableOpacity>
+				}}
+			/>
 		)
 	}
 
@@ -43,10 +41,5 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
-	},
-	gridItem: {
-		flex: 1,
-		margin: 15,
-		height: 150,
 	},
 })
