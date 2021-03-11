2969741a3ed7b95fcdbbc79a095666f1d2e31066
Chris Paterson <chrispaterson22@gmail.com>
2021-03-11 16:16:06 +0000

    added Menu button and drawer navigation

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -6,8 +6,10 @@ import {
 	FlatList,
 	TouchableOpacity,
 } from 'react-native'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 
 import { CATEGORIES } from '../data/dummy-data'
+import HeaderButton from '../components/HeaderButton'
 import CategoryGridTile from '../components/CategoryGridTile'
 
 const CategoriesScreen = props => {
@@ -30,8 +32,21 @@ const CategoriesScreen = props => {
 	)
 }
 
-CategoriesScreen.navigationOptions = {
-	headerTitle: 'Meal Categories',
+CategoriesScreen.navigationOptions = navData => {
+	return {
+		headerTitle: 'Meal Categories',
+		headerLeft: () => (
+			<HeaderButtons HeaderButtonComponent={HeaderButton}>
+				<Item
+					title='menu'
+					iconName='ios-menu'
+					onPress={() => {
+						navData.navigation.toggleDrawer()
+					}}
+				/>
+			</HeaderButtons>
+		),
+	}
 }
 
 export default CategoriesScreen
