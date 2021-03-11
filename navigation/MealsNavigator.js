84e597a228deda4d66d0021ed7128ad65c74b699
Chris Paterson <chrispaterson22@gmail.com>
2021-03-11 17:10:54 +0000

    bottom tabs styling

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Platform } from 'react-native'
+import { Platform, Text } from 'react-native'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
@@ -17,6 +17,12 @@ import Colors from '../constants/Colors'
 const defaultStackNavOptions = {
 	headerStyle: {
 		backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
+	},
+	headerTitleStyle: {
+		fontFamily: 'open-sans-bold',
+	},
+	headerBackTitleStyle: {
+		fontFamily: 'open-sans',
 	},
 	headerTintColor: Platform.OS === 'android' ? '#fff' : Colors.primaryColor,
 }
@@ -52,16 +58,27 @@ const tabScreenConfig = {
 				)
 			},
 			tabBarColor: Colors.primaryColor,
+			tabBarLabel:
+				Platform.OS === 'android' ? (
+					<Text style={{ fontFamily: 'open-sans' }}>Meals</Text>
+				) : (
+					'Meals'
+				),
 		},
 	},
 	Favorites: {
 		screen: FavNavigator,
 		navigationOptions: {
-			tabBarLabel: 'Favorites!',
 			tabBarIcon: tabInfo => {
 				return <Ionicons name='ios-heart' size={25} color={tabInfo.tintColor} />
 			},
 			tabBarColor: Colors.accentColor,
+			tabBarLabel:
+				Platform.OS === 'android' ? (
+					<Text style={{ fontFamily: 'open-sans' }}>Favorites</Text>
+				) : (
+					'Favorites'
+				),
 		},
 	},
 }
@@ -74,6 +91,9 @@ const MealsFavTabNavigator =
 		  })
 		: createBottomTabNavigator(tabScreenConfig, {
 				tabBarOptions: {
+					labelStyle: {
+						fontFamily: 'open-sans',
+					},
 					activeTintColor: 'Colors.accentColor',
 				},
 		  })
@@ -106,7 +126,7 @@ const MainNavigator = createDrawerNavigator(
 			activeTintColor: Colors.accentColor,
 			labelStyle: {
 				fontFamily: 'open-sans-bold',
-				fontWeight: 'normal',
+				fontWeight: 'normal', //added to allow font to work on android
 			},
 		},
 	}
